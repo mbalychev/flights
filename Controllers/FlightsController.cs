7f209bf4d59995c693af0a7cd5ffaa688d55f92b
mikhail_balychev <mikhail_balychev@prof-p.ru>
2023-06-01 16:56:46 +0300

    filter flights

--- Controllers/FlightsController.cs
+++ Controllers/FlightsController.cs
@@ -33,11 +33,21 @@ namespace flights.Controllers
         }
 
         /// <summary>
-        /// список полетов
+        /// список полетов (пагинация, фильтры)
         /// </summary>
+        /// <param name="pagination">пагинация</param>
+        /// <param name="arrival">место прибытия</param>
+        /// <param name="status">статус рейса</param>
+        /// <param name="scheduledArriveMin">мин время прибытия</param>
+        /// <param name="scheduledArriveMax">макс время прибытия</param>
         /// <returns></returns>
         [HttpPost("")]
-        public async Task<ActionResult<IEnumerable<Flight>>> GetTModels(Pagination? pagination)
+        public async Task<ActionResult<IEnumerable<Flight>>> GetTModels(
+            Pagination? pagination,
+            string? arrival,
+            string? status,
+            DateTime? scheduledArriveMin,
+            DateTime? scheduledArriveMax)
         {
             if (!pagination.CheckPagination())
                 return NotFound(PaginationsExtensions.BadPaginationMessage());
@@ -45,7 +55,16 @@ namespace flights.Controllers
             if (pagination is null)
                 pagination = new Pagination();
 
+            //значения по умолчанию
+            scheduledArriveMin = (scheduledArriveMin is null) ? await _context.Flights.Select(x => x.ScheduledArrival).MinAsync() : scheduledArriveMin;
+            scheduledArriveMax = (scheduledArriveMax is null) ? await _context.Flights.Select(x => x.ScheduledArrival).MaxAsync() : scheduledArriveMax;
+
             ICollection<Flight> flights = await _context.Flights
+                .OrderBy(x => x.FlightId)
+                .Where(x => x.ScheduledArrival >= scheduledArriveMin &&
+                        x.ScheduledArrival <= scheduledArriveMax &&
+                        x.ArrivalAirport == arrival &&
+                        x.Status == status)
                 .Skip(pagination.OnPage * (pagination.Page - 1))
                 .Take(pagination.OnPage)
                 .ToListAsync();
@@ -54,7 +73,7 @@ namespace flights.Controllers
 
             FlightsView model = new FlightsView(flights, pagination);
 
-            return Ok(JsonSerializer.Serialize(model));
+            return Ok(model);
         }
 
         /// <summary>
